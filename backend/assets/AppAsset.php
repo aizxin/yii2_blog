0074408a7ecce503f8a2678578360fdfe172ae43
aizxin <aizxin@gmail.com>
2017-01-12 08:50:16 +0800

    静态资源

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,7 +12,14 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        "media/css/bootstrap.min.css",
+        "media/css/bootstrap-responsive.min.css",
+	    "media/css/font-awesome.min.css",
+	    "media/css/style-metro.css",
+	    "media/css/style.css",
+	    "media/css/style-responsive.css",
+	    "media/css/default.css",
+	    "media/css/uniform.default.css",
     ];
     public $js = [
     ];
